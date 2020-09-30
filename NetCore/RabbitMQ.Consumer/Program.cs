7ec51b8e1b77d239f903803bf95cb183d7029301
Carlos Rendon <carlos@zen-it.solutions>
2020-10-01 03:53:16 +1000

    net core instructions commit

---
+++ NetCore/RabbitMQ.Consumer/Program.cs
@@ -0,0 +1,38 @@
+﻿using RabbitMQ.Client;
+using System;
+using RabbitMQ.Client.Events;
+using System.Collections.Generic;
+using System.Text;
+namespace RabbitMQ.Consumer
+{
+    static class Program
+    {
+        static void Main(string[] args)
+        {
+            var factory = new ConnectionFactory
+            {
+                Uri = new Uri("amqps://gxtsnvrg:W1Paf_bB1m5og1b-llWs2kJZl8DDcw7K@elephant.rmq.cloudamqp.com/gxtsnvrg")
+            };
+            using var connection = factory.CreateConnection();
+            using var channel = connection.CreateModel();
+            channel.QueueDeclare("codingtest",
+                durable: true,
+                exclusive: false,
+                autoDelete: false,
+                arguments: null);
+
+            var consumer = new EventingBasicConsumer(channel);
+            consumer.Received += (sender, e) => {
+                var body = e.Body.ToArray();
+                var message = Encoding.UTF8.GetString(body);
+                Console.WriteLine(message);
+            };
+
+            channel.BasicConsume("codingtest", true, consumer);
+            Console.WriteLine("Consumer started");
+            Console.ReadLine();
+
+            //FanoutExchangeConsumer.Consume(channel);
+        }
+    }
+}
---
+++ NetCore/RabbitMQ.Producer/Program.cs
@@ -0,0 +1,48 @@
+﻿using RabbitMQ.Client;
+using System;
+using Newtonsoft.Json;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+
+namespace RabbitMQ.Producer
+{
+    static class Program
+    {
+        static void Main(string[] args)
+        {
+            var factory = new ConnectionFactory
+            {
+                Uri = new Uri("amqps://gxtsnvrg:W1Paf_bB1m5og1b-llWs2kJZl8DDcw7K@elephant.rmq.cloudamqp.com/gxtsnvrg")
+            };
+            //using var connection = factory.CreateConnection();
+            IConnection conn = factory.CreateConnection();
+            IModel channel = conn.CreateModel();
+            //using var channel = connection.CreateModel();
+
+            channel.QueueDeclare("codingtest",
+                durable: true,
+                exclusive: false,
+                autoDelete: false,
+                arguments: null);
+
+            byte[] messageBodyBytes = System.Text.Encoding.UTF8.GetBytes("Hello, world!");
+            channel.BasicPublish("", "codingtest", null, messageBodyBytes);
+            
+            //channel.Close();
+            //conn.Close();
+
+            //var message = new { Name = "Producer", Message = $"Hello! Count: {count}" };
+            //var body = Encoding.UTF8.GetBytes(JsonConvert.SerializeObject(message));
+
+            //var properties = channel.CreateBasicProperties();
+            //properties.Headers = new Dictionary<string, object> { { "account", "update" } };
+
+            //channel.BasicPublish("demo-fanout-exchange", "account.new", properties, body);
+
+
+            //FanoutExchangePublisher.Publish(channel);
+
+        }
+    }
+}
